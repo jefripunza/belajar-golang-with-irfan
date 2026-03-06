bc358657f6e29799de6f8c16c02cc2aa9140dd66
jefripunza <jefripunza@gmail.com>
2026-03-06 18:06:50 +0700

    feat: add user management service methods and make User.id optional
    - Add getAllUsers, editUser, and removeUser functions to user.service.ts
    - Make User.id field optional in user type definition
    - Add TODO comment for management section

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -7,3 +7,27 @@ export const getMe = async () => {
     await satellite.get<Response<{ user: User }>>("/api/user/me");
   return response.data.data.user;
 };
+
+// TODO: Management
+
+export const getAllUsers = async () => {
+  const response = await satellite.get<Response<{ users: User[] }>>(
+    "/api/user/manage/all",
+  );
+  return response.data.data.users;
+};
+
+export const editUser = async (id: string, data: Partial<User>) => {
+  const response = await satellite.put<Response<{ user: User }>>(
+    `/api/user/manage/${id}`,
+    data,
+  );
+  return response.data.data.user;
+};
+
+export const removeUser = async (id: string) => {
+  const response = await satellite.delete<Response<unknown>>(
+    `/api/user/manage/${id}`,
+  );
+  return response.data.data;
+};
